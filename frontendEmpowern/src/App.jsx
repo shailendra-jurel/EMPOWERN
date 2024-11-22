de813c85cd26f975a86705aadc8d67b3f3461306
shailendra-jurel <shailendrajurel001@gmail.com>
2024-11-22 22:17:10 +0530

    Adding  Labor/Employee  MainPage  but have some coflicts to resolve::

--- frontendEmpowern/src/App.jsx
+++ frontendEmpowern/src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
 
+import MainPage from './Components/labor-work-flow/mainPage.jsx'
+
 function App() {
 
   return (
     <>
      <h1>   
        start builid fronted soon !!
+       <MainPage/>
       </h1>
     </>
   )
